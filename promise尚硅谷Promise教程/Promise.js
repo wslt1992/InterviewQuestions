c8c1da746cf1dd6d854a3cacc62c66ea5ed2130c
wslt1992 <465275024@qq.com>
2020-09-11 13:00:52 +0800

    promise构造函数

--- promise尚硅谷Promise教程/Promise.js
+++ promise尚硅谷Promise教程/Promise.js
@@ -4,7 +4,59 @@
 
 (function(params){
     function Promise(executor){
+        const self = this;
+        self.status = 'pending' //给promise对象指定status属性，初始值为pending
+        self.data = undefined  //给promise指定一个用于存储结果的数据属性
+        self.callbacks = []  //每个元素结构 {onResolved(){}, onRejected(){}}
 
+        function resolve(value){
+        //    如果当前状态不是pending，直接结束
+            if(self.status!=='pending'){
+                return
+            }
+
+        //    状态改为resolved
+            self.status = 'resolved'
+        //    保存value数据
+            self.value = value
+        //    如果有待执行callback函数，立即异步执行回调
+            if(self.callbacks.length){
+                self.callbacks.forEach(callbackObj=>{
+                    // 异步调用
+                    setTimeout(()=>{
+                        callbackObj.onResolved(value)
+                    },0)
+                })
+            }
+        }
+
+        function reject(reason){
+            //    如果当前状态不是pending，直接结束
+            if(self.status!=='pending'){
+                return
+            }
+
+            //    状态改为resolved
+            self.status = 'resolved'
+            //    保存value数据
+            self.data = reason
+            //    如果有待执行callback函数，立即异步执行回调
+            if(self.callbacks.length){
+                self.callbacks.forEach(callbackObj=>{
+                    // 异步调用
+                    setTimeout(()=>{
+                        callbackObj.onRejeted(reason)
+                    },0)
+                })
+            }
+        }
+
+        try{
+            executor(resolve, reject)
+        }catch(error){
+            // 如果执行器抛出异常，promise对象变为rejected状态
+            reject(error)
+        }
     }
 
     /*
@@ -12,8 +64,12 @@
     * 指定成功和失败的
     * 返回一个新的promise对象
     * */
-    Promise.prototype.then = function(onResolve, onReject){
-
+    Promise.prototype.then = function(onResolved, onRejected){
+        const self = this;
+        self.callbacks.push({
+            onResolved,
+            onRejected
+        })
     }
 
     /*
